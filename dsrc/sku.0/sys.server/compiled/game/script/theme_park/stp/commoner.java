b5898ddd333bffaa8872f30cebe2b517150f54dd
Roachie <freddylikespotatos@gmail.com>
2020-09-03 09:20:58 -0500

    Refactor cantina script and commoner script

---
+++ dsrc/sku.0/sys.server/compiled/game/script/theme_park/stp/commoner.java
@@ -0,0 +1,29 @@
+package script.theme_park.stp;
+
+import script.library.ai_lib;
+import script.obj_id;
+/**
+ *
+ * @author Roachie
+ * @purpose Randomize commoners names dynamically on start up to give a less static feel to the city.
+ */
+public class commoner extends script.base_script {
+    public static String[] NAMES = {
+        "a farmer",
+        "a smuggler",
+        "an artisan",
+        "a medic",
+        "a farmhand",
+        "a field worker"
+    };
+    
+    public commoner() {
+    }
+    
+    public int OnInitialize(obj_id self) throws InterruptedException {
+        setInvulnerable(self, true);
+        setName(self, NAMES[rand(0,5)]);
+        ai_lib.setDefaultCalmBehavior(self, 1);
+        return SCRIPT_CONTINUE;
+    }
+}
--- dsrc/sku.0/sys.server/compiled/game/script/theme_park/stp/stp_cantina_access.java
+++ dsrc/sku.0/sys.server/compiled/game/script/theme_park/stp/stp_cantina_access.java
@@ -11,30 +11,18 @@ public class stp_cantina_access extends script.base_script {
     
     public int OnAboutToReceiveItem(obj_id self, obj_id destinationCell, obj_id transferrer, obj_id item) throws InterruptedException
     {
-        
         if (!isPlayer(item))
         {
             return SCRIPT_CONTINUE;
         }
-        obj_id r1 = getCellId(self, "back_hallway");
-        obj_id r2 = getCellId(self, "back_entrance");
-        if (destinationCell != r1 || destinationCell != r2)
+        int gating = getIntObjVar(item, "stp_cantina_access");
+        if (gating != 1)
         {
-            return SCRIPT_CONTINUE;
-        }
-        int access_var = getIntObjVar(item, "stp.cantina_access");
-        if (access_var != 1)
-        {
-            sendSystemMessageTestingOnly(item, "You do not have access to this part of the cantina.");
+            sendSystemMessageTestingOnly(item, "You don't have permission to go back here. It is probably best if you turn around.");
             return SCRIPT_OVERRIDE;
         }
         else 
         {
-            if (!hasObjVar(item, "stp.cantina_message_shown"))
-            {
-                sendSystemMessageTestingOnly(item, "You now have access to these rooms.");
-                setObjVar(item, "stp.cantina_message_shown", 1);
-            }
             return SCRIPT_CONTINUE;
         }
     }
