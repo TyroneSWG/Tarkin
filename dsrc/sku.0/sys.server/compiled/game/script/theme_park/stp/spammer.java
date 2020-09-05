03de47b77889c74733299249e4a32c17e750fac2
Roachie <freddylikespotatos@gmail.com>
2020-09-05 12:07:54 -0500

    scanner:

---
+++ dsrc/sku.0/sys.server/compiled/game/script/theme_park/stp/spammer.java
@@ -0,0 +1,26 @@
+package script.theme_park.stp;
+
+import script.*;
+import script.library.chat;
+/**
+ *
+ * @author Roachie
+ */
+public class spammer extends script.base_script {
+    public spammer() {
+    }
+    public static int OnAttach(obj_id self) throws InterruptedException
+    {
+        createTriggerVolume("hub_spammer", 15.0f, true);
+        return SCRIPT_CONTINUE;
+    }
+    public int OnTriggerVolumeEntered(obj_id self, String volumeName, obj_id breacher) throws InterruptedException
+    {
+        if (!isPlayer(breacher))
+        {
+            return SCRIPT_CONTINUE;
+        }
+        chat.chat(self, "Target Locked... Scanning " + getPlayerName(breacher));
+        return SCRIPT_CONTINUE;
+    }
+}
