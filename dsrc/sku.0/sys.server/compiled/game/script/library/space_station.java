912e71675cd9665ff0bc81cc609b9cd18026e4fd
Roachie <freddylikespotatos@gmail.com>
2020-09-05 08:20:42 -0500

    Redoing the space stations properly this time

--- dsrc/sku.0/sys.server/compiled/game/script/library/space_station.java
+++ dsrc/sku.0/sys.server/compiled/game/script/library/space_station.java
@@ -1,11 +1,14 @@
 package script.library;
 
 import script.*;
+import script.location;
 import script.library.*;
 
 /**
- *
+ * @name space_station.java
  * @author Roachie
+ * @purpose Library for new space station hub. Similar to that of the Imperial and Republic Fleet in SWTOR.
+ * used for commerce, hanging out, some small quests, and overall new hangout area.
  */
 public class space_station extends script.base_script {
     public space_station() {
@@ -14,7 +17,11 @@ public class space_station extends script.base_script {
     public int moveToHub(obj_id player) throws InterruptedException
     {
         obj_id planet = getPlanetByName("dungeon_hub");
-        loc planetBase = getLocation(planet);
+        if (isIdValid(planet))
+        {
+            return SCRIPT_CONTINUE;
+        }
+        location planetBase = getLocation(planet);
         obj_id[] stations = getAllObjectsWithTemplate(planetBase, 16000.0f, HUB_TEMPLATE);
         obj_id[] stationCells = getCellIds(stations[0]);
         warpPlayer(player, "dungeon_hub", 0.0f, 0.0f, 0.0f, stationCells[0], 0.0f, 0.0f, 0.0f);
