35b3e8fac027a85aa6e65e32beb693069a314e3d
Roachie <freddylikespotatos@gmail.com>
2020-09-15 11:21:41 -0500

    trigger droid

--- dsrc/sku.0/sys.server/compiled/game/script/theme_park/stp/spammer.java
+++ dsrc/sku.0/sys.server/compiled/game/script/theme_park/stp/spammer.java
@@ -11,7 +11,7 @@ public class spammer extends script.base_script {
     }
     public static int OnAttach(obj_id self) throws InterruptedException
     {
-        createTriggerVolume("hub_spammer", 15.0f, true);
+        createTriggerVolume("hub_spammer", 5.0f, false);
         return SCRIPT_CONTINUE;
     }
     public int OnTriggerVolumeEntered(obj_id self, String volumeName, obj_id breacher) throws InterruptedException
@@ -22,7 +22,17 @@ public class spammer extends script.base_script {
         }
         if (volumeName.equals("hub_spammer"))
         {
-            chat.chat(self, "Target Locked... Scanning " + getPlayerName(breacher));
+            chat.chat(self, "Scanning Target....");
+            chat.chat(self, "Target locked: " + getFirstName(breacher).toUpperCase());
+            if (hasObjVar(breacher, "hub.generator_quest"))
+            {
+                chat.chat(self, "Hostile target determined. Intializing Combat Routine");
+                startCombat(self, breacher);
+            }
+            else
+            {
+                chat.chat(self, "No hostile targets found. Recycling...");
+            }
         }
         return SCRIPT_CONTINUE;
     }
