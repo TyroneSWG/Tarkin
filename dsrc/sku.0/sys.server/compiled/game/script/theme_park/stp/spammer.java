56626ef64573db70f19f3e94423ec5201754edd4
Roachie <freddylikespotatos@gmail.com>
2020-09-15 11:26:30 -0500

    trigger droid

--- dsrc/sku.0/sys.server/compiled/game/script/theme_park/stp/spammer.java
+++ dsrc/sku.0/sys.server/compiled/game/script/theme_park/stp/spammer.java
@@ -2,6 +2,7 @@ package script.theme_park.stp;
 
 import script.*;
 import script.library.chat;
+import script.library.groundquests;
 /**
  *
  * @author Roachie
@@ -22,16 +23,19 @@ public class spammer extends script.base_script {
         }
         if (volumeName.equals("hub_spammer"))
         {
-            chat.chat(self, "Scanning Target....");
-            chat.chat(self, "Target locked: " + getFirstName(breacher).toUpperCase());
-            if (hasObjVar(breacher, "hub.generator_quest"))
+            if (groundquests.isQuestActive(breacher, "quest/taan_preq"))
             {
-                chat.chat(self, "Hostile target determined. Intializing Combat Routine");
-                startCombat(self, breacher);
-            }
-            else
-            {
-                chat.chat(self, "No hostile targets found. Recycling...");
+                chat.chat(self, "Scanning Target....");
+                chat.chat(self, "Target locked: " + getFirstName(breacher).toUpperCase());
+                if (hasObjVar(breacher, "hub.generator_quest"))
+                {
+                   chat.chat(self, "Hostile target determined. Intializing Combat Routine");
+                    startCombat(self, breacher);
+                }
+                else
+                {
+                    chat.chat(self, "No hostile targets found. Recycling...");
+                }
             }
         }
         return SCRIPT_CONTINUE;
