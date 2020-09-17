14139cc9ce7623687d6417dc8b18426579341a71
Roachie <freddylikespotatos@gmail.com>
2020-09-17 14:06:57 -0500

    Boombox

--- dsrc/sku.0/sys.server/compiled/game/script/theme_park/stp/wendle/boombox.java
+++ dsrc/sku.0/sys.server/compiled/game/script/theme_park/stp/wendle/boombox.java
@@ -11,23 +11,23 @@ public class boombox extends script.base_script {
     public int OnAttach(obj_id self) throws InterruptedException
     {
        createTriggerVolume("boombox_cantina", 15.0f, false);
-       setName(self, "a radio");
+       setName(self, "a radio (inactive)");
        return SCRIPT_CONTINUE;
     }
     public int OnInitialize(obj_id self) throws InterruptedException
     {
        createTriggerVolume("boombox_cantina", 15.0f, false);
-       setName(self, "a radio");
+       setName(self, "a radio (inactive)");
        return SCRIPT_CONTINUE;
     }
     public int OnTriggerVolumeEntered(obj_id self, String volumeName, obj_id breacher) throws InterruptedException
     {
         if (isPlayer(breacher) && (volumeName.equals("boombox_cantina")))
         {
-            String soundFile = "music/mus_pirate_cantina.mp3";
-            String soundFile2 = "music/mus_cantina_band_remix.mp3";
+            String soundFile = "sound/mus_pirate_cantina.snd";
+            String soundFile2 = "sound/mus_cantina_band_remix.snd";
             setName(self, getName(self) + " (active)");
-            play2dNonLoopingMusic(breacher, soundFile);
+            play2dNonLoopingSound(breacher, soundFile);
             return SCRIPT_CONTINUE;
         }
         return SCRIPT_CONTINUE;
