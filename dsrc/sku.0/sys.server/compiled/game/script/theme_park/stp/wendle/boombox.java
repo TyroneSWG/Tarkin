618c49d5fe44f33588dd477e3e8712128ad76465
Roachie <freddylikespotatos@gmail.com>
2020-09-17 14:03:50 -0500

    Boombox

--- dsrc/sku.0/sys.server/compiled/game/script/theme_park/stp/wendle/boombox.java
+++ dsrc/sku.0/sys.server/compiled/game/script/theme_park/stp/wendle/boombox.java
@@ -15,34 +15,18 @@ public class boombox extends script.base_script {
     }
     public int OnInitialize(obj_id self) throws InterruptedException
     {
-       createTriggerVolume("boombox_cantina", 25.0f, true);
+       createTriggerVolume("boombox_cantina", 15.0f, false);
        setName(self, "a radio");
        return SCRIPT_CONTINUE;
     }
     public int OnTriggerVolumeEntered(obj_id self, String volumeName, obj_id breacher) throws InterruptedException
     {
-        if (!isPlayer(breacher))
-        {
-            return SCRIPT_CONTINUE;
-        }
-        if (volumeName.equals("boombox_cantina"))
+        if (isPlayer(breacher) && (volumeName.equals("boombox_cantina")))
         {
             String soundFile = "music/mus_pirate_cantina.mp3";
             String soundFile2 = "music/mus_cantina_band_remix.mp3";
-            int whichOne = rand(0,100);
-            if (whichOne <= 50)
-            {
-                setName(self, getName(self) + "(Now Playing: Pirate Swing)");
-                play2dNonLoopingMusic(breacher, soundFile);
-            }
-            if (whichOne >= 50)
-            {
-                setName(self, getName(self) + "(Now Playing: Cantina Remix)");
-                play2dNonLoopingMusic(breacher, soundFile2);
-            }
-        }
-        else
-        {
+            setName(self, getName(self) + " (active)");
+            play2dNonLoopingMusic(breacher, soundFile);
             return SCRIPT_CONTINUE;
         }
         return SCRIPT_CONTINUE;
